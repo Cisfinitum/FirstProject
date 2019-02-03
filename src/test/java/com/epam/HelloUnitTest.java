8fcf8a1b523d02061521f91ec9969c6f510f3394
igrigoriev <igrigoriev@roox.ru>
2019-02-03 23:20:00 +0300

    JUnit library and simple unit test added

---
+++ src/test/java/com/epam/HelloUnitTest.java
@@ -0,0 +1,10 @@
+package com.epam;
+
+import org.junit.jupiter.api.Test;
+
+public class HelloUnitTest {
+        @Test
+        public void testSomething() {
+                System.out.println("HelloUnitTest");
+        }
+}
