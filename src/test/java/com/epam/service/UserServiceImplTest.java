7aaba9a52a59a792a756411016e51b2f995d74e1
MaltsevaKsenia <maltseva.ksenia01@gmal.com>
2019-02-10 01:12:50 +0300

    add annotation before

--- src/test/java/com/epam/service/UserServiceImplTest.java
+++ src/test/java/com/epam/service/UserServiceImplTest.java
@@ -1,6 +1,7 @@
 package com.epam.service;
 
 import com.epam.entity.User;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
@@ -10,11 +11,16 @@ import static org.junit.jupiter.api.Assertions.*;
 class UserServiceImplTest {
     @Mock
     private User user;
+    private UserServiceImpl userService;
+
+    @BeforeEach
+    void setUp() {
+        userService = new UserServiceImpl();
+        MockitoAnnotations.initMocks(this);
+    }
 
     @Test
     void getUser() {
-        MockitoAnnotations.initMocks(this);
-        UserServiceImpl userService = new UserServiceImpl();
         when(user.getUsername()).thenReturn("John");
         User actualUser = userService.getUser(user.getUsername());
         User expectedUser = userService.getUser("John");
