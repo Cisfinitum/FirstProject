3ba580c7f5bbae01e6bd89899788c63f9be66603
MaltsevaKsenia <maltseva.ksenia01@gmal.com>
2019-02-11 14:15:20 +0300

    fix codacy

--- src/test/java/com/epam/service/UserServiceImplTest.java
+++ src/test/java/com/epam/service/UserServiceImplTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+
 import static org.mockito.Mockito.*;
 import static org.junit.jupiter.api.Assertions.*;
 
