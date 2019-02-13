ca88024b50696f07287acde6a8f00343cac3d787
MaltsevaKsenia <maltseva.ksenia01@gmal.com>
2019-02-13 16:09:50 +0300

    raise percent of test coverage

--- src/test/java/com/epam/service/PersonDetailsServiceImplTest.java
+++ src/test/java/com/epam/service/PersonDetailsServiceImplTest.java
@@ -2,54 +2,62 @@ package com.epam.service;
 
 import com.epam.model.Person;
 import com.epam.model.PersonRoleEnum;
-import com.epam.repository.PersonDAO;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashSet;
+import java.util.Set;
 
 import static org.mockito.Mockito.when;
 import static org.junit.jupiter.api.Assertions.*;
 
+
 class PersonDetailsServiceImplTest {
-
     @Mock
-    private PersonDAO personDAO;
-
-    private List<Person> personList;
     private PersonService personService;
-    private Person expectedPerson;
+    @Mock
+    private Person person;
+
+    private Set<GrantedAuthority> roles;
+    private PersonDetailsServiceImpl personDetailsServiceImpl;
 
     @BeforeEach
     void setUp() {
         MockitoAnnotations.initMocks(this);
-        expectedPerson= new Person(1, "user", "1111", PersonRoleEnum.valueOf("ADMIN"));
-        personService = new PersonService(personDAO);
-        personList = new ArrayList<>();
-        personList.add(expectedPerson);
+        personDetailsServiceImpl = new PersonDetailsServiceImpl(personService);
+        roles = new HashSet<>();
     }
 
     @Test
-    void getPersonPositiveResult() {
-        String name = "user";
-        when(personDAO.getPersons()).thenReturn(personList);
-
-        Person actualPerson = personService.getPerson(name);
-
-        assertEquals(expectedPerson, actualPerson);
+    void loadUserPositiveResult() {
+        String username = "user";
+        String password = "123";
+        when(personService.getPerson(username)).thenReturn(new Person(1, username, password, PersonRoleEnum.ADMIN));
+        when(person.getRole()).thenReturn(PersonRoleEnum.ADMIN);
+        roles.add(new SimpleGrantedAuthority(person.getRole().getEnumrole()));
+        UserDetails expectedPerson = new User(username, password, roles);
+        UserDetails actualPerson = personDetailsServiceImpl.loadUserByUsername(username);
+        assertEquals(expectedPerson.getUsername(), actualPerson.getUsername());
     }
+
     @Test
-    void getPersonNotFound() {
-        String name = "tmp";
-        when(personDAO.getPersons()).thenReturn(personList);
-
-        Person actualPerson = personService.getPerson(name);
-
-        assertNull(actualPerson);
+    void loadUserThrowsException() {
+        String nonExistUsername = "1111";
+        when(personService.getPerson(nonExistUsername)).thenThrow(UsernameNotFoundException.class);
+        when(person.getRole()).thenReturn(PersonRoleEnum.ADMIN);
+        roles.add(new SimpleGrantedAuthority(person.getRole().getEnumrole()));
+        try {
+            personDetailsServiceImpl.loadUserByUsername(nonExistUsername);
+            fail();
+        } catch (UsernameNotFoundException thrown) {
+            assertNotNull(thrown);
+        }
     }
-
-
 }
---
+++ src/test/java/com/epam/service/PersonServiceTest.java
@@ -0,0 +1,49 @@
+package com.epam.service;
+
+import com.epam.model.Person;
+import com.epam.model.PersonRoleEnum;
+import com.epam.repository.PersonDAO;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+
+class PersonServiceTest {
+    @Mock
+    private PersonDAO personDAO;
+
+    private List<Person> personList;
+    private PersonService personService;
+    private Person expectedPerson;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.initMocks(this);
+        expectedPerson = new Person(1, "user", "1111", PersonRoleEnum.valueOf("ADMIN"));
+        personService = new PersonService(personDAO);
+        personList = new ArrayList<>();
+        personList.add(expectedPerson);
+    }
+
+    @Test
+    void getPersonPositiveResult() {
+        String name = "user";
+        when(personDAO.getPersons()).thenReturn(personList);
+        Person actualPerson = personService.getPerson(name);
+        assertEquals(expectedPerson, actualPerson);
+    }
+
+    @Test
+    void getPersonNotFound() {
+        String name = "tmp";
+        when(personDAO.getPersons()).thenReturn(personList);
+        Person actualPerson = personService.getPerson(name);
+        assertNull(actualPerson);
+    }
+}
