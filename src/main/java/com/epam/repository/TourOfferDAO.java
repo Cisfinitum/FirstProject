44e13a2f098797def90246bb28a459f3fa7dcf53
VNTee <kevdin@mail.ru>
2019-02-15 15:41:07 +0300

    Tours

--- src/main/java/com/epam/repository/TourOfferDAO.java
+++ src/main/java/com/epam/repository/TourOfferDAO.java
@@ -2,24 +2,41 @@ package com.epam.repository;
 
 import com.epam.model.TourOffer;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Repository;
 
+import java.sql.Date;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
 
 @Repository
 public class TourOfferDAO {
-    private final NamedParameterJdbcTemplate jdbcTemplate;
+    private final JdbcTemplate simpleJdbcTemplate;
 
     @Autowired
-    public TourOfferDAO(NamedParameterJdbcTemplate jdbcTemplate) {
-        this.jdbcTemplate = jdbcTemplate;
+    public TourOfferDAO(JdbcTemplate simpleJdbcTemplate) {
+        this.simpleJdbcTemplate = simpleJdbcTemplate;
     }
 
     public List<TourOffer> getTours(){
-        return jdbcTemplate.query("SELECT * FROM tourOffer", (rs, rowNum) -> buildTour(rs));
+        return simpleJdbcTemplate.query("SELECT FROM tourOffer", (rs, rowNum) -> buildTour(rs));
+    }
+
+    public int deleteTour(int tourId){
+         return simpleJdbcTemplate.update("DELETE FROM tourOffer WHERE id = ?", tourId);
+    }
+
+    public int addTour(String tourType, Date startDate, Date endDate, int pricePerUnit, int hotel_id, String description, int discount_id){
+        return simpleJdbcTemplate.update("INSERT INTO tourOffer(tourType, startDate, endDate, pricePerUnit, hotel_id, description, discount_id) " +
+                "VALUES (?, ?, ?, ?, ?, ?, ?)",tourType,startDate,endDate,pricePerUnit,hotel_id,description,discount_id);
+    }
+
+    public int updateTour(String tourType, Date startDate, Date endDate, int pricePerUnit, int hotel_id, String description, int discount_id, int tourId){
+       return simpleJdbcTemplate.update("UPDATE tourOffer SET " +
+               "tourType = ?, startDate = ?, endDate = ?, pricePerUnit = ?, hotel_id = ?, description = ?, discount_id = ? " +
+               "WHERE id = ?",tourType,startDate,endDate,pricePerUnit,hotel_id,description,discount_id,tourId);
     }
 
     TourOffer buildTour(ResultSet rs) throws SQLException {
--- src/main/java/com/epam/service/TourOfferService.java
+++ src/main/java/com/epam/service/TourOfferService.java
@@ -5,6 +5,7 @@ import com.epam.repository.TourOfferDAO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.sql.Date;
 import java.util.List;
 
 @Service
@@ -17,8 +18,24 @@ public class TourOfferService {
         this.tourOfferDAO = tourOfferDAO;
     }
 
-    public List<TourOffer> getToursByParams(){
-        List<TourOffer> tours = tourOfferDAO.getTours();
-        return tours;
+    /// TODO replace (int hotelId) on (List<Integer> listHotels)
+    public List<TourOffer> getTours(int hotelId, Date startDate, int numberOfDays, int numberOfPeople){
+ //       List<Person> persons = personDAO.getPersons();
+ //       for (Person person: persons) {
+ //           if (person.getName().equals(name)) return person;
+ //       }
+        return null;
+    }
+
+    public int deleteTour(int tourId){
+         return tourOfferDAO.deleteTour(tourId);
+    }
+
+    public int addTour(String tourType, Date startDate, Date endDate, int pricePerUnit, int hotel_id, String description, int discount_id){
+        return tourOfferDAO.addTour(tourType,startDate,endDate,pricePerUnit,hotel_id,description,discount_id);
+    }
+
+    public int updateTour(String tourType, Date startDate, Date endDate, int pricePerUnit, int hotel_id, String description, int discount_id, int tourId){
+        return tourOfferDAO.updateTour(tourType,startDate,endDate,pricePerUnit,hotel_id,description,discount_id,tourId);
     }
 }
