0314030ad70a758eea91f6f8a93bb2ef8edf2c04
VNTee <kevdin@mail.ru>
2019-02-17 10:30:32 +0300

    Additional test

--- src/test/java/com/epam/service/TourOfferServiceTest.java
+++ src/test/java/com/epam/service/TourOfferServiceTest.java
@@ -22,21 +22,24 @@ public class TourOfferServiceTest {
     private Date teststartDate = Date.valueOf("2018-02-19");
     private Date testendDate = Date.valueOf("2018-02-25");
     private Integer testpricePerUnit = 1500;
-    private Integer testhotel_id = 1;
+    private Integer testhotelId = 1;
     private String testdescription = "Best tour";
-    private Integer testdiscount_id = 1;
+    private Integer testdiscountid = 1;
     private List<TourOffer> tourOfferList;
     private TourOfferService tourOfferService;
     private int expectedResultPositive = 1;
+    private List<Integer> testHotelIdList = new ArrayList<>();
+    private String testCountry = "Turkey";
 
     @Before
     public void setUp() {
         MockitoAnnotations.initMocks(this);
         expectedTourOffer = new TourOffer(1, testtourType, teststartDate,  testendDate,
-                testpricePerUnit, testhotel_id,testdescription, testdiscount_id);
+                testpricePerUnit, testhotelId,testdescription, testdiscountid);
         tourOfferService = new TourOfferService(tourOfferDAO);
         tourOfferList = new ArrayList<>();
         tourOfferList.add(expectedTourOffer);
+        testHotelIdList.add(testhotelId);
     }
 
     @Test
@@ -58,7 +61,7 @@ public class TourOfferServiceTest {
     public void addTourCheck(){
         when(tourOfferDAO.addTour(expectedTourOffer)).thenReturn(1);
         int actualResulat = tourOfferService.addTour(1, testtourType, teststartDate,  testendDate,
-                testpricePerUnit, testhotel_id,testdescription, testdiscount_id);
+                testpricePerUnit, testhotelId,testdescription, testdiscountid);
         assertEquals(actualResulat,expectedResultPositive);
     }
 
@@ -66,8 +69,15 @@ public class TourOfferServiceTest {
     public void updateTourCheck(){
         when(tourOfferDAO.updateTour(expectedTourOffer)).thenReturn(1);
         int actualResulat = tourOfferService.updateTour(1, testtourType, teststartDate,  testendDate,
-                testpricePerUnit, testhotel_id,testdescription, testdiscount_id);
+                testpricePerUnit, testhotelId,testdescription, testdiscountid);
         assertEquals(actualResulat,expectedResultPositive);
     }
+    @Test
+    public void searchTourCheck(){
+        when(tourOfferDAO.searchTours(testHotelIdList,teststartDate,testendDate)).thenReturn(tourOfferList);
+        List<TourOffer> actualResulat = tourOfferService.searchTours(testCountry,teststartDate,testendDate);
+        assertEquals(actualResulat,tourOfferList);
+    }
+
 }
 
