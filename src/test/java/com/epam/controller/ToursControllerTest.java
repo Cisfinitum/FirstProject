4dea60615937690d0866de4ad7f534b432b652b9
VNTee <kevdin@mail.ru>
2019-02-18 14:11:12 +0300

    Ask Ksu

---
+++ src/test/java/com/epam/controller/ToursControllerTest.java
@@ -0,0 +1,33 @@
+package com.epam.controller;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ToursControllerTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void getToursList() {
+    }
+
+    @Test
+    public void searchTours() {
+    }
+
+    @Test
+    public void deleteTour() {
+    }
+
+    @Test
+    public void addTour() {
+    }
+
+    @Test
+    public void updateTour() {
+    }
+}
