8955209e4e4d27eb4ef2945bc9a4ba4d0ba5c931
Ilia Grigorev <grigorieviv@gmail.com>
2019-02-19 23:02:23 +0300

    minor fixes of validation and unit tests

--- src/main/java/com/epam/service/HotelService.java
+++ src/main/java/com/epam/service/HotelService.java
@@ -20,38 +20,38 @@ public class HotelService {
 
     public List<Hotel> getHotels(){ return hotelDAO.getHotels(); }
 
-    public List<Hotel> getHotelsByCountry (String country) throws Exception {
+    public List<Hotel> getHotelsByCountry (String country) {
         if (country != null)
         return hotelDAO.getHotelsByCountry(country);
-        else throw new Exception("Country must be specified");
+        else throw new IllegalArgumentException("Country must be specified");
     }
 
-    public Hotel getHotelById (Integer id) throws Exception {
+    public Hotel getHotelById (Integer id) {
         if (id != null)
         return hotelDAO.getHotelById(id);
-        else throw new Exception("Id must be specified");
+        else throw new IllegalArgumentException("Id must be specified");
     }
 
-    public Integer createHotel(Hotel hotel) throws Exception {
+    public Integer createHotel(Hotel hotel) {
         if (hotel.getName() != null) {
             if (hotel.getCountry() != null)
                 return hotelDAO.createHotel(hotel);
-            else throw new Exception("Country must be specified");
-        } else throw new Exception("Name must be specified");
+            else throw new IllegalArgumentException("Country must be specified");
+        } else throw new IllegalArgumentException("Name must be specified");
     }
 
-    public Integer updateHotel (Hotel hotel) throws Exception {
+    public Integer updateHotel (Hotel hotel) {
         if (hotel.getName() != null) {
             if (hotel.getCountry() != null)
                 return hotelDAO.updateHotel(hotel);
-            else throw new Exception("Country must be specified");
-        } else throw new Exception("Name must be specified");
+            else throw new IllegalArgumentException("Country must be specified");
+        } else throw new IllegalArgumentException("Name must be specified");
     }
 
-    public Integer deleteHotel (Integer id) throws Exception{
+    public Integer deleteHotel (Integer id) {
         if (id != null)
             return hotelDAO.deleteHotel(id);
-        else throw new Exception("Id must be specified");
+        else throw new IllegalArgumentException("Id must be specified");
     }
 
 }
--- src/test/java/com/epam/service/HotelServiceTest.java
+++ src/test/java/com/epam/service/HotelServiceTest.java
@@ -2,8 +2,10 @@ package com.epam.service;
 
 import com.epam.model.Hotel;
 import com.epam.repository.HotelDAO;
+import lombok.SneakyThrows;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
@@ -16,50 +18,62 @@ import static org.mockito.Mockito.when;
 public class HotelServiceTest {
     @Mock
     private HotelDAO hotelDAO;
+    @Mock
     private Hotel expectedHotel;
     private List<Hotel> hotelList;
+    @InjectMocks
     private HotelService hotelService;
     private int expectedResultPositive = 1;
-
-    private Integer testId = 1;
-    private String testHotelName = "Luxury Hotel";
-    private String testHotelCity = "Moscow";
-    private String testHotelCountry = "Russia";
-    private Integer testHotelStars = 5;
 
     @Before
     public void setUp() {
         MockitoAnnotations.initMocks(this);
 
-        expectedHotel =  new Hotel(1, "Luxury Hotel", "Moscow", "Russia",5);
-        hotelService = new HotelService(hotelDAO);
         hotelList = new ArrayList<>();
         hotelList.add(expectedHotel);
+        when(expectedHotel.getName()).thenReturn("Luxury Hotel");
+        when(expectedHotel.getCountry()).thenReturn("Russia");
     }
 
     @Test
     public void getHotels() {
         when(hotelDAO.getHotels()).thenReturn(hotelList);
         List<Hotel> actualResult = hotelService.getHotels();
-        assertEquals(actualResult,hotelList);
+        assertEquals(hotelList, actualResult);
+    }
+
+    @Test (expected = IllegalArgumentException.class)
+    @SneakyThrows
+    public void getHotelsByCountryThrowsException() {
+        hotelService.getHotelsByCountry(null);
     }
 
     @Test
-    public void createHotel()throws Exception{
+    @SneakyThrows
+    public void createHotel() {
         when(hotelDAO.createHotel(expectedHotel)).thenReturn(1);
         int actualResult = hotelService.createHotel(expectedHotel);
         assertEquals(actualResult,expectedResultPositive);
     }
 
+    @Test(expected = IllegalArgumentException.class)
+    @SneakyThrows
+    public void createHotelThrowsException() {
+        when(expectedHotel.getName()).thenReturn(null);
+        hotelService.createHotel(expectedHotel);
+    }
+
     @Test
-    public void updateHotel() throws Exception{
+    @SneakyThrows
+    public void updateHotel() {
         when(hotelDAO.updateHotel(expectedHotel)).thenReturn(1);
         int actualResult = hotelService.updateHotel(expectedHotel);
         assertEquals(actualResult,expectedResultPositive);
     }
 
     @Test
-    public void deleteHotel() throws Exception {
+    @SneakyThrows
+    public void deleteHotel() {
         when(hotelDAO.deleteHotel(1)).thenReturn(1);
         int actualResult = hotelService.deleteHotel(1);
         assertEquals(actualResult,expectedResultPositive);
