fccee4dacae4eca3781a52f97926eaa987a16b00
MaltsevaKsenia <maltseva.ksenia01@gmal.com>
2019-02-20 17:20:44 +0300

    edit names of columns according to convention in data

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -2,9 +2,9 @@ INSERT INTO person(email,  password, role)
 VALUES ('user@gmail.com', '123','ADMIN');
 INSERT INTO discount(discount)
 VALUES ('50');
-INSERT INTO hotel(name, city, country, numberOfStars)
+INSERT INTO hotel(name, city, country, number_of_stars)
 VALUES ('Luxury Hotel', 'Moscow', 'Russia', '5');
-INSERT INTO tourOffer(tourType, startDate, endDate, pricePerUnit, hotel_id, description, discount_id)
+INSERT INTO tourOffer(tour_type, start_date, end_date, price_per_unit, hotel_id, description, discount_id)
 VALUES ('Shopping',CURRENT_DATE, CURRENT_DATE, '500000', '1', 'best tour ever', '1');
-INSERT INTO reservation(client_id, tourOffer_id, numberOfPersons, status, discount_id, totalPrice)
+INSERT INTO reservation(client_id, tour_offer_id, number_of_persons, status, discount_id, total_price)
 VALUES ('1','1', '1', 'paid', '1', '500000');
