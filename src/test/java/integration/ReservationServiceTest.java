e564eb99c4d25928c40659d47bf05720a03f69be
MaltsevaKsenia <maltseva.ksenia01@gmal.com>
2019-02-20 20:47:57 +0300

    correct tests

--- src/test/java/integration/ReservationServiceTest.java
+++ src/test/java/integration/ReservationServiceTest.java
@@ -18,23 +18,26 @@ import java.util.stream.Collectors;
 @RunWith(SpringRunner.class)
 @ContextConfiguration(classes = TestConfig.class)
 public class ReservationServiceTest {
-
     @Autowired
     private ReservationService reservationService;
 
     @Autowired
     private ReservationDAO reservationDAO;
 
+    private int testPage = 1;
+    private int testTotal = 5;
+
     @Test
     public void getAll() {
-        List<Reservation> actualReservations = reservationService.listReservations();
+
+        List<Reservation> actualReservations = reservationService.listReservations(testPage, testTotal);
         Assert.assertNotEquals(0, actualReservations.size());
     }
 
     @Test(expected = EmptyResultDataAccessException.class)
     public void removeReservation() {
         reservationDAO.addReservation(new Reservation(10, 1, 1, 5, ReservationStatusEnum.PAID, 10, 20));
-        List<Reservation> reservations = reservationDAO.listReservations();
+        List<Reservation> reservations = reservationDAO.listReservations(testPage, testTotal);
         List<Reservation> reservationsFromDb = reservations.stream().filter(reservation -> reservation.getNumberOfPeople().equals(5)).collect(Collectors.toList());
         Integer reservationId = reservationsFromDb.get(0).getId();
         reservationService.removeReservation(reservationId);
