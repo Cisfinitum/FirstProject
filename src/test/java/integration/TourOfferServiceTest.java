bb2656290403c457676735a56f43425d9fc02e66
Nikolai Imbirev <nikolai_imbirev@epam.com>
2019-02-23 21:05:51 +0300

    add integration tests for TourOfferService, add necessary exception for notFoundException, invert all if conditions from TourOfferService

--- src/test/java/integration/TourOfferServiceTest.java
+++ src/test/java/integration/TourOfferServiceTest.java
@@ -1,8 +1,10 @@
 package integration;
 
+import com.epam.exception.NotFoundException;
 import com.epam.model.TourOffer;
 import com.epam.repository.TourOfferDAO;
 import com.epam.service.TourOfferService;
+import java.time.LocalDate;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
@@ -49,8 +51,69 @@ public class TourOfferServiceTest {
         Assert.assertEquals(0, actualTours.size());
     }
 
+    @Test(expected = IllegalArgumentException.class)
+    public void deleteTourNullId() {
+        tourOfferService.deleteTour(null);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void deleteTourZeroId() {
+        tourOfferService.deleteTour(0);
+    }
+
     @Test
     public void deleteTour() {
+        int tourOfferId = 10;
+        Mockito.when(jdbcTemplate.update(Mockito.anyString(), (Integer) Mockito.any())).thenReturn(1);
 
+        int actualReturnCode = tourOfferService.deleteTour(tourOfferId);
+        Assert.assertEquals(1, actualReturnCode);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void addTourNull() {
+        tourOfferService.addTour(null);
+    }
+
+//    @Test(expected = IllegalArgumentException.class)
+//    public void addTourOfferWrongHotelId() {
+//        Mockito.when(tourOffer.getHotelId()).thenReturn(0);
+//
+//        tourOfferService.addTour(tourOffer);
+//
+//        Mockito.verifyZeroInteractions(jdbcTemplate);
+//    }
+//    @Test(expected = IllegalArgumentException.class)
+//    public void addTourOfferWrongTourType() {
+//        Mockito.when(tourOffer.getTourType()).thenReturn("");
+//
+//        tourOfferService.addTour(tourOffer);
+//
+//        Mockito.verifyZeroInteractions(jdbcTemplate);
+//    }
+
+//    @Test(expected = NotFoundException.class)
+//    public void updateTourNotFoundInTheDatabase() {
+//        Mockito.when(tourOffer.getId()).thenReturn(1);
+//        Mockito.when(tourOffer.getHotelId()).thenReturn(2);
+//        Mockito.when(jdbcTemplate.update(Mockito.anyString(), (Object[]) Mockito.any())).thenReturn(0);
+//
+//        tourOfferService.updateTour(tourOffer);
+//    }
+
+    @Test
+    public void updateTour() {
+        Mockito.when(tourOffer.getId()).thenReturn(1);
+        Mockito.when(tourOffer.getHotelId()).thenReturn(1);
+        Mockito.when(tourOffer.getTourType()).thenReturn("dd");
+        Mockito.when(tourOffer.getDiscountId()).thenReturn(1);
+        Mockito.when(tourOffer.getEndDate()).thenReturn(LocalDate.of(2019, 10, 11));
+
+        tourOfferService.updateTour(tourOffer);
+
+        Mockito.verify(jdbcTemplate, Mockito.times(1)).update("UPDATE tourOffer SET " +
+                "tourType = ?, startDate = ?, endDate = ?, pricePerUnit = ?, hotel_id = ?, description = ?, discount_id = ? WHERE id = ?",
+            tourOffer.getTourType(), tourOffer.getStartDate(), tourOffer.getEndDate(), tourOffer.getPricePerUnit(), tourOffer.getHotelId(),
+            tourOffer.getDescription(), tourOffer.getDiscountId(), tourOffer.getId());
     }
 }
