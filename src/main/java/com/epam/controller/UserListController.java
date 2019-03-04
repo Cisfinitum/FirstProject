d78d4e62df0efb945ab7eb36648ff48064cde682
Lev <lev.kiselev.1769@gmail.com>
2019-03-04 11:44:30 +0300

    little fix

--- src/main/java/com/epam/controller/UserListController.java
+++ src/main/java/com/epam/controller/UserListController.java
@@ -6,6 +6,8 @@ import com.epam.service.PersonDetailsServiceImpl;
 import com.epam.service.ReservationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
