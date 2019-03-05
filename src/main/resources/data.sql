4510ba243c41c0cf87da119ff621b52abf9d4cb4
VNTee <kevdin@mail.ru>
2019-03-05 18:51:48 +0300

    Add data for tours

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -27,13 +27,13 @@ VALUES ('Iron axe', 'Minas Tirith', 'Gondor', '4');
 INSERT INTO hotel(name, city, country, number_of_stars)
 VALUES ('White walker', 'Edoras', 'Rohan', '3');
 INSERT INTO hotel(name, city, country, number_of_stars)
-VALUES ('Leaky pot', 'Aldburg', 'Rohan', '5');
+VALUES ('Leaky pot', 'Aldburg', 'Rohan', '1');
 INSERT INTO hotel(name, city, country, number_of_stars)
-VALUES ('Three pigs', 'Dunharrow', 'Rohan', '4');
+VALUES ('Three pigs', 'Dunharrow', 'Rohan', '1');
 INSERT INTO hotel(name, city, country, number_of_stars)
 VALUES ('Cheshire cat', 'Hornburg', 'Rohan', '3');
 INSERT INTO hotel(name, city, country, number_of_stars)
-VALUES ('Old brewer', 'Osgiliath', 'Gondor', '5');
+VALUES ('Old brewer', 'Osgiliath', 'Gondor', '2');
 INSERT INTO hotel(name, city, country, number_of_stars)
 VALUES ('Fairy and ale', 'Minas Ithil', 'Gondor', '4');
 INSERT INTO hotel(name, city, country, number_of_stars)
@@ -45,7 +45,7 @@ VALUES ('Gnarled hooks', 'Cair Andros', 'Gondor', '4');
 INSERT INTO hotel(name, city, country, number_of_stars)
 VALUES ('MO', 'Barad-dur', 'Mordor', '3');
 INSERT INTO hotel(name, city, country, number_of_stars)
-VALUES ('Middle-earth booze', 'Durthang', 'Mordor', '5');
+VALUES ('Middle-earth booze', 'Durthang', 'Mordor', '1');
 INSERT INTO hotel(name, city, country, number_of_stars)
 VALUES ('Pitched troll', 'Towers of the Teet', 'Mordor', '4');
 INSERT INTO hotel(name, city, country, number_of_stars)
@@ -53,33 +53,33 @@ VALUES ('Lame pegasus', 'Cirith Ungol', 'Mordor', '3');
 INSERT INTO hotel(name, city, country, number_of_stars)
 VALUES ('Crown', 'Lorien', 'The Misty Mountains', '3');
 INSERT INTO hotel(name, city, country, number_of_stars)
-VALUES ('Divine rapier', 'Mirkwood', 'Mirkwood', '3');
+VALUES ('Divine rapier', 'Mirkwood', 'Mirkwood', '2');
 INSERT INTO hotel(name, city, country, number_of_stars)
-VALUES ('Hidden in leaf', 'Fangorn', 'The Misty Mountains', '3');
+VALUES ('Hidden in leaf', 'Fangorn', 'The Misty Mountains', '5');
 INSERT INTO hotel(name, city, country, number_of_stars)
-VALUES ('Enchanted mango', 'Isengard', 'Gondor', '3');
+VALUES ('Enchanted mango', 'Isengard', 'Gondor', '5');
 INSERT INTO hotel(name, city, country, number_of_stars)
-VALUES ('Fairy fire', 'Moria', 'The Misty Mountains', '3');
+VALUES ('Fairy fire', 'Moria', 'The Misty Mountains', '2');
 INSERT INTO hotel(name, city, country, number_of_stars)
-VALUES ('Magic stick', 'Esgaroth', 'Esgaroth', '3');
+VALUES ('Magic stick', 'Esgaroth', 'Esgaroth', '1');
 INSERT INTO hotel(name, city, country, number_of_stars)
 VALUES ('Honningbrew meadery', 'Rivendell', 'Rivendell', '3');
 INSERT INTO hotel(name, city, country, number_of_stars)
-VALUES ('Black-briar meadery', 'Mountains of Lune', 'Mountains of Lune', '3');
+VALUES ('Black-briar meadery', 'Mountains of Lune', 'Mountains of Lune', '4');
 INSERT INTO hotel(name, city, country, number_of_stars)
-VALUES ('Silvermoon', 'Lonely Mountain', 'Lonely Mountain', '3');
+VALUES ('Silvermoon', 'Lonely Mountain', 'Lonely Mountain', '2');
 INSERT INTO hotel(name, city, country, number_of_stars)
 VALUES ('Prancing pony', 'Bree', 'Bree', '3');
 INSERT INTO tour_offer(tour_type, start_date, end_date, price_per_unit, hotel_id, description, discount)
-VALUES ('Hunter','2019-03-05', '2019-03-10', '100000', '2', 'Gondor tour', '0');
+VALUES ('Hunt','2019-03-05', '2019-03-10', '100000', '2', 'Stone rat hunt', '0');
 INSERT INTO tour_offer(tour_type, start_date, end_date, price_per_unit, hotel_id, description, discount)
-VALUES ('Active','2019-03-06', '2019-03-19', '200000', '1', 'text', '0');
+VALUES ('Active','2019-03-06', '2019-03-19', '200000', '1', 'Wild warg walk', '0');
 INSERT INTO tour_offer(tour_type, start_date, end_date, price_per_unit, hotel_id, description, discount)
-VALUES ('Active','2019-03-07', '2019-03-13', '300000', '3', 'text', '0');
+VALUES ('Active','2019-03-07', '2019-03-13', '300000', '3', 'Hippogriph racing', '0');
 INSERT INTO tour_offer(tour_type, start_date, end_date, price_per_unit, hotel_id, description, discount)
-VALUES ('Entertaiment','2019-03-08', '2019-03-15', '400000', '5', 'text', '50');
+VALUES ('Entertainment','2019-03-08', '2019-03-15', '400000', '5', 'A feast in honor of the Day of Sweets', '50');
 INSERT INTO tour_offer(tour_type, start_date, end_date, price_per_unit, hotel_id, description, discount)
-VALUES ('Shopping','2019-03-09', '2019-03-14', '500000', '1', 'text', '50');
+VALUES ('Shopping','2019-03-09', '2019-03-14', '500000', '1', 'Attend the interracial fair', '50');
 INSERT INTO reservation(client_id, tour_offer_id, number_of_people, archive_status,status, discount, total_price)
 VALUES ('1','1', '1', 'NEW','PAID', '1', '500000');
 INSERT INTO reservation(client_id, tour_offer_id, number_of_people,archive_status, status, discount, total_price)
