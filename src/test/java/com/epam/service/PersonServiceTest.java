fef1b09cc742b5eb2fcb51a4dc32a7ce4d31110a
Ekaterina Goponenko <goponenko.yekaterina@yandex.ru>
2019-03-06 13:09:51 +0300

    update* tests

--- src/test/java/com/epam/service/PersonServiceTest.java
+++ src/test/java/com/epam/service/PersonServiceTest.java
@@ -1,58 +1,65 @@
-//package com.epam.service;
-//
-//
-//import com.epam.model.Person;
-//import com.epam.model.PersonRoleEnum;
-//import com.epam.repository.PersonDAO;
-//import org.junit.Before;
-//import org.junit.Test;
-//import org.mockito.Mock;
-//import org.mockito.MockitoAnnotations;
-//
-//import java.util.ArrayList;
-//import java.util.List;
-//
-//import static org.junit.Assert.assertEquals;
-//import static org.junit.Assert.assertNull;
-//import static org.mockito.Mockito.when;
-//
-//public class PersonServiceTest {
-//    @Mock
-//    private PersonDAO personDAO;
-//
-//
-//    public static final int EXPECTED_RESULT = 1;
-//    public static final String NULL_ARGUMENT = null;
-//    private List<Person> personList;
-//    private PersonService personService;
-//    private Person expectedPerson;
-//    private String testEmail;
-//    private String testPassword;
-//    private Integer testId;
-//    private String testPhoneNumber = "8999999999";
-//    private String testFirstName = "Example";
-//    private String testLastName = "Example";
-//
-//    @Before
-//    public void setUp() {
-//        MockitoAnnotations.initMocks(this);
-//        testId = 1;
-//        testEmail = "user";
-//        testPassword = "1111";
-//        expectedPerson = new Person(testId, testEmail, testPassword, PersonRoleEnum.valueOf("ADMIN"), testPhoneNumber, testFirstName, testLastName);
-//        personService = new PersonService(personDAO);
-//        personList = new ArrayList<>();
-//        personList.add(expectedPerson);
-//    }
-//
-//    @Test
-//    public void getPersonPositiveResult() {
-//        String email = "user";
-//        when(personDAO.getPersons()).thenReturn(personList);
-//        Person actualPerson = personService.getPerson(email);
-//        assertEquals(expectedPerson, actualPerson);
-//    }
-//
+package com.epam.service;
+
+
+import com.epam.exception.InvalidDataBaseAffectedException;
+import com.epam.model.Person;
+import com.epam.model.PersonRoleEnum;
+import com.epam.repository.PersonDAO;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.when;
+
+public class PersonServiceTest {
+    @Mock
+    private PersonDAO personDAO;
+
+    @Mock
+    private BCryptPasswordEncoder testPasswordEncoder;
+
+
+    private static final int EXPECTED_RESULT = 1;
+    private static final int UNEXPECTED_RESULT = 10;
+    private static final String NULL_ARGUMENT = null;
+    private List<Person> personList;
+    private PersonService personService;
+    private Person expectedPerson;
+    private String testEmail;
+    private String testPassword;
+    private Integer testId;
+    private String testPhoneNumber = "8999999999";
+    private String testFirstName = "Example";
+    private String testLastName = "Example";
+
+    @Before
+    public void setUp() {
+        MockitoAnnotations.initMocks(this);
+        testId = 1;
+        testEmail = "user";
+        testPassword = "1111";
+        expectedPerson = new Person(testId, testEmail, testPassword, PersonRoleEnum.valueOf("ADMIN"), testPhoneNumber, testFirstName, testLastName);
+        personService = new PersonService(personDAO, testPasswordEncoder);
+        personList = new ArrayList<>();
+        personList.add(expectedPerson);
+    }
+
+    @Test
+    public void getPersonPositiveResult() {
+        String email = "user";
+        when(personDAO.getPersons()).thenReturn(personList);
+        Person actualPerson = personService.getPerson(email);
+        assertEquals(expectedPerson, actualPerson);
+    }
+
 //    @Test
 //    public void getPersonNotFound() {
 //        String name = "isNotExist";
@@ -77,19 +84,62 @@
 //    public void addPersonNullArgument() {
 //        personService.getPerson(null);
 //    }
-//
-//    @Test
-//    public void updatePasswordPositiveResult() {
-//        int expectedResult = 1;
-//        when(personDAO.updatePassword(testEmail, testPassword)).thenReturn(1);
-//        assertEquals(expectedResult, personService.updatePassword(testEmail, testPassword));
-//    }
-//
-//    @Test(expected = IllegalArgumentException.class)
-//    public void updatePasswordEmailIsNull() {
-//        personService.updatePassword(null, testPassword);
-//    }
-//
+
+    @Test
+    public void updatePasswordPositiveResult() {
+        when(personDAO.updatePassword(testEmail, testPassword)).thenReturn(EXPECTED_RESULT);
+        assertTrue(personService.updatePassword(testEmail, testPassword));
+    }
+
+    @Test
+    public void updatePasswordNegativeResult() {
+        when(personDAO.updatePassword(testEmail, testPassword)).thenReturn(-1);
+        assertFalse(personService.updatePassword(testEmail, testPassword));
+    }
+
+    @Test(expected = InvalidDataBaseAffectedException.class)
+    public void updatePasswordMoreThanOneRow() {
+        when(personDAO.updatePassword(testEmail, testPassword)).thenReturn(UNEXPECTED_RESULT);
+        personService.updatePassword(testEmail, testPassword);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void updatePasswordEmailIsNull() {
+        personService.updatePassword(NULL_ARGUMENT, testPassword);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void updatePasswordPassIsNull() {
+        personService.updatePassword(testEmail, NULL_ARGUMENT);
+    }
+
+    @Test
+    public void updatePasswordByIdPositiveResult() {
+        when(personDAO.updatePasswordById(testId, testPassword)).thenReturn(EXPECTED_RESULT);
+        assertTrue(personService.updatePasswordById(testId, testPassword));
+    }
+
+    @Test
+    public void updatePasswordByIdNegativeResult() {
+        when(personDAO.updatePasswordById(testId, testPassword)).thenReturn(-1);
+        assertFalse(personService.updatePasswordById(testId, testPassword));
+    }
+
+    @Test(expected = InvalidDataBaseAffectedException.class)
+    public void updatePasswordByIdMoreThanOneRow() {
+        when(personDAO.updatePasswordById(testId, testPassword)).thenReturn(UNEXPECTED_RESULT);
+        personService.updatePasswordById(testId, testPassword);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void updatePasswordByIdsNull() {
+        personService.updatePasswordById(null, testPassword);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void updatePasswordByPasswordsNull() {
+        personService.updatePasswordById(testId, NULL_ARGUMENT);
+    }
 //    @Test
 //    public void addToBlackListPositiveResult() {
 //        int expectedResult = 1;
@@ -138,45 +188,100 @@
 //    public void getIdByEmailNullEmail() {
 //        personService.getIdByEmail("");
 //    }
-//
-//    @Test
-//    public void updatePhoneNumberPositiveResult() {
-//        when(personDAO.updatePhoneNumberById(testId, testPhoneNumber)).thenReturn(EXPECTED_RESULT);
-//        assertEquals(EXPECTED_RESULT, personService.updatePhoneNumberById(testId, testPhoneNumber));
-//    }
-//
-//    @Test(expected = IllegalArgumentException.class)
-//    public void updatePhoneNumberNullArgument() {
-//        personService.updatePhoneNumberById(testId, NULL_ARGUMENT);
-//    }
-//
-//    @Test
-//    public void updateFirstNamePositiveResult() {
-//        when(personDAO.updateFirstNameById(testId, testFirstName)).thenReturn(EXPECTED_RESULT);
-//        assertEquals(EXPECTED_RESULT, personService.updateFirstNameById(testId, testFirstName));
-//    }
-//
-//    @Test(expected = IllegalArgumentException.class)
-//    public void updateFirstNameNullArgument() { personService.updateFirstNameById(testId, NULL_ARGUMENT);
-//    }
-//
-//    @Test
-//    public void updateLastNamePositiveResult() {
-//        when(personDAO.updateLastNameById(testId, testLastName)).thenReturn(EXPECTED_RESULT);
-//        assertEquals(EXPECTED_RESULT, personService.updateLastNameById(testId, testLastName));
-//    }
-//
-//    @Test(expected = IllegalArgumentException.class)
-//    public void updateLastNameNullArgument() { personService.updateLastNameById(testId, NULL_ARGUMENT);
-//    }
-//
-//    @Test
-//    public void updateEmailPositiveResult() {
-//        when(personDAO.updateEmailById(testId, testEmail)).thenReturn(EXPECTED_RESULT);
-//        assertEquals(EXPECTED_RESULT, personService.updateEmailById(testId, testEmail));
-//    }
-//
-//    @Test(expected = IllegalArgumentException.class)
-//    public void updateEmailNullArgument() { personService.updateEmailById(testId, NULL_ARGUMENT);
-//    }
-//}
+
+    @Test
+    public void updatePhoneNumberPositiveResult() {
+        when(personDAO.updatePhoneNumberById(testId, testPhoneNumber)).thenReturn(EXPECTED_RESULT);
+        assertTrue(personService.updatePhoneNumberById(testId, testPhoneNumber));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void updatePhoneNumberNullArgument() {
+        personService.updatePhoneNumberById(testId, NULL_ARGUMENT);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void updatePhoneNumberIdNullArgument() {
+        personService.updatePhoneNumberById(null, testPhoneNumber);
+    }
+
+    @Test(expected = InvalidDataBaseAffectedException.class)
+    public void updatePhoneNumberDataBaseException() {
+        when(personDAO.updatePhoneNumberById(testId, testPhoneNumber)).thenReturn(UNEXPECTED_RESULT);
+        personService.updatePhoneNumberById(testId, testPhoneNumber);
+    }
+
+    @Test
+    public void updatePhoneNumberInvalidId() {
+        when(personDAO.updatePhoneNumberById(testId, testPassword)).thenReturn(0);
+        assertFalse(personService.updatePhoneNumberById(testId, testPassword));
+    }
+
+    @Test
+    public void updateFirstNamePositiveResult() {
+        when(personDAO.updateFirstNameById(testId, testFirstName)).thenReturn(EXPECTED_RESULT);
+        assertTrue(personService.updateFirstNameById(testId, testFirstName));
+    }
+
+    @Test
+    public void updateFirstNameNegativeResult() {
+        when(personDAO.updateFirstNameById(testId, testFirstName)).thenReturn(0);
+        assertFalse(personService.updateFirstNameById(testId, testFirstName));
+    }
+
+    @Test(expected = InvalidDataBaseAffectedException.class)
+    public void updateFirstNamePositiveResultInvalid() {
+        when(personDAO.updateFirstNameById(testId, testFirstName)).thenReturn(UNEXPECTED_RESULT);
+        personService.updateFirstNameById(testId, testFirstName);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void updateFirstNameNullArgument() {
+        personService.updateFirstNameById(testId, NULL_ARGUMENT);
+    }
+
+    @Test
+    public void updateLastNamePositiveResult() {
+        when(personDAO.updateLastNameById(testId, testLastName)).thenReturn(EXPECTED_RESULT);
+        assertTrue( personService.updateLastNameById(testId, testLastName));
+    }
+
+    @Test
+    public void updateLastNameNegativeResult() {
+        when(personDAO.updateLastNameById(testId, testLastName)).thenReturn(0);
+        assertFalse( personService.updateLastNameById(testId, testLastName));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void updateLastNameNullArgument() {
+        personService.updateLastNameById(testId, NULL_ARGUMENT);
+    }
+    @Test(expected = InvalidDataBaseAffectedException.class)
+    public void updateLastNameDataBaseException() {
+        when(personDAO.updateLastNameById(testId, testLastName)).thenReturn(UNEXPECTED_RESULT);
+        personService.updateLastNameById(testId, testLastName);
+    }
+
+    @Test
+    public void updateEmailPositiveResult() {
+        when(personDAO.updateEmailById(testId, testEmail)).thenReturn(EXPECTED_RESULT);
+        assertTrue(personService.updateEmailById(testId, testEmail));
+    }
+
+    @Test
+    public void updateEmailNegativeResult() {
+        when(personDAO.updateEmailById(testId, testEmail)).thenReturn(0);
+        assertFalse(personService.updateEmailById(testId, testEmail));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void updateEmailNullArgument() {
+        personService.updateEmailById(testId, NULL_ARGUMENT);
+    }
+
+    @Test(expected = InvalidDataBaseAffectedException.class)
+    public void updateEmailDBException() {
+        when(personDAO.updateEmailById(testId, testEmail)).thenReturn(UNEXPECTED_RESULT);
+        personService.updateEmailById(testId, testEmail);
+    }
+}
